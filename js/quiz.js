e03488e4d9bee69b25b99b6fae66e65a56bc7e99
Shirasu <shirasu0124ss@gmail.com>
2024-06-03 14:03:28 +0900

    リファクタリング

--- js/quiz.js
+++ js/quiz.js
@@ -62,24 +62,6 @@ function checkAnswer() {
     }
 }
 
-(function($) {
-    var $nav   = $('#navArea');
-    var $btn   = $('.toggle_btn');
-    var $mask  = $('#mask');
-    var open   = 'open'; // class
-    // menu open close
-    $btn.on( 'click', function() {
-    if ( ! $nav.hasClass( open ) ) {
-        $nav.addClass( open );
-    } else {
-        $nav.removeClass( open );
-    }
-    });
-    // mask close
-    $mask.on('click', function() {
-    $nav.removeClass( open );
-    });
-} )(jQuery);
 
 // クリック時に記事の表示を切り替える関数
 function toggle(element) {
